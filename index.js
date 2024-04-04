6bbe5297f88e729156d120bcc76d2659fbfe0f92
tubisaffo <tubisaffo@gmail.com>
2024-04-04 11:36:02 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+// dodger.style.backgroundColor = '#000000';
+// dodger.style.backgroundColor = '#FF69B4';
+
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//         const leftNumbers = dodger.style.left.replace("px", "");
+//         const left = parseInt(leftNumbers, 10);
+//         dodger.style.left = `${left - 1}px`;
+
+//     }
+
+// });
+
+
+function moveDodgerLeft () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+}
+
+}
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
